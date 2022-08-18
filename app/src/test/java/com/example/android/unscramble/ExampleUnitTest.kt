e9d433c879effc5f7c61182a5566cc5c2bec5abe
Pasha831 <pashamedvedev03@gmail.com>
2022-08-18 18:00:41 +0300

    add crucial unit test

---
+++ app/src/test/java/com/example/android/unscramble/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.example.android.unscramble
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
